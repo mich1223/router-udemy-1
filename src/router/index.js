a3282c12fc3053598bc5e000e5d7086d0d03b87b
mich <michelle@cceo.com.mx>
2022-08-08 17:12:36 -0500

    Rutas con Parámetros

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ const routes = [
     path: '/blog',
     name: 'Blog',
     component: () => import(/* webpackChunkName: "about" */ '../views/Blog.vue')
+  },
+  {
+    path: '/blog/:id',
+    name: 'Articulo',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Articulo.vue')
   }
 ]
 
