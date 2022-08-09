f55c9e04806656f90ddee0dbe17bb7cd79100809
mich <michelle.adame@cceo.com.mx>
2022-08-09 15:27:32 -0500

    version final, bug reparado

--- src/views/Articulo.vue
+++ src/views/Articulo.vue
@@ -1,37 +1,39 @@
 <template>
-   <Titulo texto="Ruta con parámetros"/>
-   <h2>Parámetro: {{ $route.params.id }}</h2>
+  <Titulo texto="Artículo específico" />
+  <h2>Parámetro: {{ $route.params.id }}</h2>
+  <h3>{{ articulo.title }}</h3>
+  <hr>
+  <p>{{ articulo.body }}</p>
 </template>
 
 <script>
-import Titulo from '../components/Titulo.vue'
-export default{
-    components:{
+import Titulo from '../components/Titulo'
+export default {
+    components: {
         Titulo
     },
-    data(){
+    data() {
         return {
             articulo: {}
         }
     },
-    methods:{
-        async consumirArticulo(){
-            try{
-                const data=await fetch(`https://jsonplaceholder.typicode.com/posts/${this.$route.params.id}`) /*extrae los datod de la api */
-                const objeto=await data.json()
-                console.log(objeto)
-
-            }catch(error){
+    methods: {
+        async getArticulo() {
+            try {
+                console.log(this.$route.params.id)
+                const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${this.$route.params.id}`)
+                const objeto = await data.json()
+                this.articulo = objeto;
+            } catch (error) {
                 console.log(error)
             }
         }
     },
-    create(){
-        this.consumirArticulo()
+    created() {
+        this.getArticulo()
     }
 }
 </script>
-
 <style >
 
 </style>
