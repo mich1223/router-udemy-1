14dfda0e98cd7409a0e9814b45caf80838582900
mich <michelle.adame@cceo.com.mx>
2022-08-09 15:51:26 -0500

    Final

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,7 +3,32 @@
     <Titulo texto="Sobre Nosotros"/>
   </div>
 </template>
-
+import Titulo from '../components/Titulo'
+export default {
+    components: {
+        Titulo
+    },
+    data() {
+        return {
+            articulo: {}
+        }
+    },
+    methods: {
+        async getArticulo() {
+            try {
+                console.log(this.$route.params.id)
+                const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${this.$route.params.id}`)
+                const objeto = await data.json()
+                this.articulo = objeto;
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    },
+    created() {
+        this.getArticulo()
+    }
+}
 
 <script >
 import Titulo from '../components/Titulo.vue'
